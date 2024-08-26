70e5a876bc652718eb4737410d6b231fa522a861
Mārtiņš Karogs <Martins.Karogs@proton.me>
2024-08-26 17:37:09 +0300

    first commands

--- mcli/__main__.py
+++ mcli/__main__.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
+
+import click
+
+
+@click.group()
+def cli():
+	pass
+
+
+@click.command()
+@click.argument('message')
+def encrypt(message):
+	click.echo((message))
+
+
+@click.command()
+@click.argument('message')
+def decrypt(message):
+	click.echo((message))
+
+
+cli.add_command(encrypt)
+cli.add_command(decrypt)
+
 if __name__ == '__main__':
-	print('Hello world')
+	cli()
